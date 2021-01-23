6a60292f94da8137ce00be349cff2f3bc4074b61
Pedro Lana <pedroalana@hotmail.com>
2021-01-23 12:23:24 -0300

    Subindo pela primeira vez

---
+++ public/exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Cursos PHP</h1>
+        <h2>Visualização do Exercicio</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+
+        </div>
+    </main>
+    <footer class="rodape"> Pedro Lana @ 2020</footer>
+    
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu de Atividades</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Cursos PHP</h1>
+        <h2>Indice de Exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01 - Básico</h3>
+                    <ul>
+                        <li><a href="exercicio.php">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Módulo 02 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>Módulo 03 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>Módulo 04 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>Módulo 05 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>Módulo 06 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho-escuro">
+                    <h3>Módulo 07 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul-escuro">
+                    <h3>Módulo 08 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo-escuro">
+                    <h3>Módulo 09 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+            
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape"> Pedro Lana @ 2020</footer>
+    
+</body>
+</html>
