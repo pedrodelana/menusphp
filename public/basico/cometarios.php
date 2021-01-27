d86a4e10344d40145ad006f6cd70865f24a45572
Pedro Lana <you@example.com>
2021-01-27 19:01:04 -0300

    Integration php with HTML/CSS

---
+++ public/basico/cometarios.php
@@ -0,0 +1,21 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no PHP";
+// echo "Estou no PHP";
+# echo "Estou no PHP";
+echo "Estou no PHP";
+/*
+    Linha 1 do comentário
+    Linha 2 do comentário
+    Linha 3 do comentário
+*/
+?>
+
+<!--
+    <p>Cometario HTML 1</p>
+    <p>Cometario HTML 2</p>
+    <p>Cometario HTML 3</p>
+-->
+    
+
---
+++ public/basico/ola.php
@@ -0,0 +1,9 @@
+<div class="titulo">Olá PHP</div>
+
+<h3>Texto 1</h3>
+<?php
+echo "Olá";
+?>
+
+<h3>Texto 2</h3>
+<?= "Uma nova forma de se expressar!" ?>
